ff740b562b4159469c8e1ac3c91d9824faad343e
차민석 <51734158+meloncha@users.noreply.github.com>
2021-04-15 16:56:14 +0900

    [init] backend

---
+++ backend/src/main/java/creaming/CreamingApplication.java
@@ -0,0 +1,13 @@
+package creaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreamingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/creaming/CreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package creaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
