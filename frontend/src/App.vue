e692fd25d8c41d11184cd7ac0c21de3e3f3519b8
eric <eric99kr@naver.com>
2021-04-16 15:02:11 +0900

    [feat] frontend common template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-    </div>
+  <div>
+    <Home/>
   </div>
 </template>
+
+
+<script>
+import Home from '@/views/Home.vue'
+import '@/assets/css/bootstrap.min.css';
+import '@/assets/css/font-awesome.min.css';
+import '@/assets/css/elegant-icons.css';
+import '@/assets/css/magnific-popup.css';
+import '@/assets/css/nice-select.css';
+import '@/assets/css/owl.carousel.min.css';
+import '@/assets/css/slicknav.min.css';
+import '@/assets/css/style.css';
+
+
+// import "@/assets/js/jquery-3.3.1.min.js"
+// import "@/assets/js/bootstrap.min.js"
+// import "@/assets/js/jquery.nice-select.min.js"
+// import "@/assets/js/jquery.nicescroll.min.js"
+// import "@/assets/js/jquery.magnific-popup.min.js"
+// import "@/assets/js/jquery.countdown.min.js"
+// import "@/assets/js/jquery.slicknav.js"
+// import "@/assets/js/mixitup.min.js"
+// import "@/assets/js/owl.carousel.min.js"
+// import "@/assets/js/main.js"
+
+export default {
+  components :{Home},
+}
+</script>
 
 <style>
 #app {
