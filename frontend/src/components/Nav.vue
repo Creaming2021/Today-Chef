a2e8d8081ad5e9d96667ddc02706151a1c033608
성진옥 <wlsdhr0831@naver.com>
2021-04-19 14:16:39 +0900

    [feat] seperate home.vue

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -4,24 +4,10 @@
 			<div class="header__top">
 					<div class="container">
 							<div class="row">
-									<div class="col-lg-6 col-md-7">
-											<div class="header__top__left">
-													<p>Free shipping, 30-day return or refund guarantee.</p>
-											</div>
-									</div>
 									<div class="col-lg-6 col-md-5">
 											<div class="header__top__right">
 													<div class="header__top__links">
 															<a href="#">Sign in</a>
-															<a href="#">FAQs</a>
-													</div>
-													<div class="header__top__hover">
-															<span>Usd <i class="arrow_carrot-down"></i></span>
-															<ul>
-																	<li>USD</li>
-																	<li>EUR</li>
-																	<li>USD</li>
-															</ul>
 													</div>
 											</div>
 									</div>
@@ -32,7 +18,8 @@
 					<div class="row">
 							<div class="col-lg-3 col-md-3">
 									<div class="header__logo">
-											<a href="./index.html"><img src="@/assets/CREAMING-logo-white.png" alt=""></a>
+											로고자리
+											<!-- <a href="./index.html"><img src="@/assets/CREAMING-logo-white.png" alt=""></a> -->
 									</div>
 							</div>
 							<div class="col-lg-6 col-md-6">
