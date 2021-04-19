bc763f887eb32896587a67dae465eb4bc6d19707
성진옥 <wlsdhr0831@naver.com>
2021-04-19 14:54:33 +0900

    [feat] make event components used bootstrap

--- frontend/src/components/home/Event.vue
+++ frontend/src/components/home/Event.vue
@@ -1,60 +1,61 @@
 <template>
-	<!-- Hero Section Begin -->
-	<section class="hero">
-	이벤트 섹션
-			<div class="hero__slider owl-carousel">
-					<div class="hero__items set-bg" data-setbg="@/assets/img/hero/hero-1.jpg">
-							<div class="container">
-									<div class="row">
-											<div class="col-xl-5 col-lg-7 col-md-8">
-													<div class="hero__text">
-															<h6>Summer Collection</h6>
-															<h2>Fall - Winter Collections 2030</h2>
-															<p>A specialist label creating luxury essentials. Ethically crafted with an unwavering
-															commitment to exceptional quality.</p>
-															<a href="#" class="primary-btn">Shop now <span class="arrow_right"></span></a>
-															<div class="hero__social">
-																	<a href="#"><i class="fa fa-facebook"></i></a>
-																	<a href="#"><i class="fa fa-twitter"></i></a>
-																	<a href="#"><i class="fa fa-pinterest"></i></a>
-																	<a href="#"><i class="fa fa-instagram"></i></a>
-															</div>
-													</div>
-											</div>
-									</div>
-							</div>
-					</div>
-					<div class="hero__items set-bg" data-setbg="@/assets/img/hero/hero-2.jpg">
-							<div class="container">
-									<div class="row">
-											<div class="col-xl-5 col-lg-7 col-md-8">
-													<div class="hero__text">
-															<h6>Summer Collection</h6>
-															<h2>Fall - Winter Collections 2030</h2>
-															<p>A specialist label creating luxury essentials. Ethically crafted with an unwavering
-															commitment to exceptional quality.</p>
-															<a href="#" class="primary-btn">Shop now <span class="arrow_right"></span></a>
-															<div class="hero__social">
-																	<a href="#"><i class="fa fa-facebook"></i></a>
-																	<a href="#"><i class="fa fa-twitter"></i></a>
-																	<a href="#"><i class="fa fa-pinterest"></i></a>
-																	<a href="#"><i class="fa fa-instagram"></i></a>
-															</div>
-													</div>
-											</div>
-									</div>
-							</div>
-					</div>
-			</div>
-	</section>
-	<!-- Hero Section End -->
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="300"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/300/?image=52"/>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/300/?image=54"/>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/300/?image=58"/>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <!-- <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="300"
+            src="https://picsum.photos/1024/300/?image=55"
+            alt="image slot"
+          >
+        </template>
+      </b-carousel-slide> -->
+    </b-carousel>
+  </div>
 </template>
 
-
 <script>
-export default {
-}
+  export default {
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true;
+        console.log(slide);
+      },
+      onSlideEnd(slide) {
+        this.sliding = false;
+        console.log(slide);
+      }
+    }
+  }
 </script>
-
-<style>
-</style>
