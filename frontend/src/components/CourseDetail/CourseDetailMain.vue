dc4434a8300ff4e2758f6bc7d683dc0409114b92
성진옥 <wlsdhr0831@naver.com>
2021-04-21 14:54:07 +0900

    [feat] make review list in course detail
    #S04P31B204-121

--- frontend/src/components/CourseDetail/CourseDetailMain.vue
+++ frontend/src/components/CourseDetail/CourseDetailMain.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="col-lg-9 course-detail-main-container">
-    <div>
+    <div class="tab">
       <span @click="onChangeType('introduction')">클래스 소개</span> |
       <span @click="onChangeType('kit')">키트 소개</span> |
       <span @click="onChangeType('review')">리뷰</span> |
@@ -9,7 +9,9 @@
     </div>
     <CourseIntroduction v-if="this.type === 'introduction'"/>
     <CourseKit v-else-if="this.type === 'kit'"/>
-    <CourseReview v-else-if="this.type === 'review'"/>
+    <CourseReview 
+      v-else-if="this.type === 'review'"
+      :reviewList="reviewList"/>
     <CourseQnA v-else-if="this.type === 'qna'"/>
     <CourseRefund v-else-if="this.type === 'refund'"/>
   </div>
@@ -27,6 +29,69 @@ export default {
   data() {
     return {
       type: 'introduction',
+      reviewList: [
+        {
+          id: 1,
+          profile:'https://img.sbs.co.kr/newsnet/etv/upload/2021/03/05/30000673929_1280.jpg',
+          title: '리뷰 제목',
+          content: '리뷰 내용',
+          writer: '작성자 닉네임',
+          date: '2021년 4월 21일',
+          commentCnt: 2
+          // [
+          //   {
+          //     id: 1,
+          //     writer: '리뷰 작성자',
+          //     date: '2021년 4월 21일',
+          //     profile: 'https://mblogthumb-phinf.pstatic.net/MjAyMDA0MDZfMjg0/MDAxNTg2MTgwNjMxODE1.B22_kATsB4cyKg2yr9x6GoRk8OuBXutbaQdRG2wxbAMg.ZIGXNEYc2xDiH61A18wLGcNeFNl4RsDTCrK1pMY0cnIg.JPEG.b-seol/46858675_511354619382565_4518962982505897027_n(1).jpg?type=w800',
+          //     content: '리뷰 내용',
+          //   },
+          //   {
+          //     id: 2,
+          //     writer: '리뷰 작성자',
+          //     date: '2021년 4월 21일',
+          //     profile: 'https://i1.sndcdn.com/artworks-WzFnntNp1E7dHw7y-RqzlrQ-t500x500.jpg',
+          //     content: '리뷰 내용',
+          //   }
+          // ], 
+        },
+        {
+          id: 2,
+          profile:'https://t1.daumcdn.net/liveboard/styleade/b27fc94234c34dbc9240f1943cb2f0b5.png',
+          title: '리뷰 제목',
+          content: '리뷰 내용',
+          writer: '작성자 닉네임',
+          date: '2021년 4월 21일',
+          commentCnt: 0
+        },
+        {
+          id: 3,
+          profile:'https://t1.daumcdn.net/liveboard/styleade/b27fc94234c34dbc9240f1943cb2f0b5.png',
+          title: '리뷰 제목',
+          content: '리뷰 내용',
+          writer: '작성자 닉네임',
+          date: '2021년 4월 21일',
+          commentCnt: 0
+        },
+        {
+          id: 4,
+          profile:'https://t1.daumcdn.net/liveboard/styleade/b27fc94234c34dbc9240f1943cb2f0b5.png',
+          title: '리뷰 제목',
+          content: '리뷰 내용',
+          writer: '작성자 닉네임',
+          date: '2021년 4월 21일',
+          commentCnt: 0
+        },
+        {
+          id: 5,
+          profile:'https://t1.daumcdn.net/liveboard/styleade/b27fc94234c34dbc9240f1943cb2f0b5.png',
+          title: '리뷰 제목',
+          content: '리뷰 내용',
+          writer: '작성자 닉네임',
+          date: '2021년 4월 21일',
+          commentCnt: 0
+        }
+      ]
     }
   },
   components: {
@@ -52,12 +117,12 @@ export default {
   padding: 10px;
 }
 
-.course-detail-main-container div{
+.course-detail-main-container .tab{
   word-spacing: 5px;
   margin-bottom: 20px;
 }
 
-.course-detail-main-container div:hover{
+.course-detail-main-container .tab:hover{
   cursor: pointer;
 }
 </style>
