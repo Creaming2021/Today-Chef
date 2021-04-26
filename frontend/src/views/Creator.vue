cda438033b67d047bf52ad7049c2d5d874ea6070
eric <eric99kr@naver.com>
2021-04-26 22:23:53 +0900

    [feat] 강의 제작 페이지, toast UI 라이브러리 설치 #S04P31B204-152

--- frontend/src/views/Creator.vue
+++ frontend/src/views/Creator.vue
@@ -1,12 +1,36 @@
 <template>
   <div>
-		강의 제작
+  <editor
+    :initialValue="editorText"
+    :options="editorOptions"
+    height="500px"
+    initialEditType="wysiwyg"
+    previewStyle="vertical"
+  />
   </div>
 </template>
 
 
 <script>
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/vue-editor';
+
+
 export default {
+  components : {
+    editor: Editor
+  },
+  data() {
+    return {
+      editorText: 'This is initialValue.',
+      editorOptions: {
+        hideModeSwitch: true
+      }
+    };
+  },
+  mounted() {
+  }
 }
 </script>
 
