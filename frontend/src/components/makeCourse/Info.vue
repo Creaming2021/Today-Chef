9cdbd92b81a0c0e7b99ab08e9170822b1b2ff5e8
eric <eric99kr@naver.com>
2021-04-29 10:58:02 +0900

    [feat] 강의 제작 페이지 컴포넌트화 #S04P31B204-152

---
+++ frontend/src/components/makeCourse/Info.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+		간단하게 어떤 클래스인지 알려주세요
+  </div>
+</template>
+
+<script>
+	export default {
+		components: {
+	},
+	data(){
+		return {
+		}
+	},
+  mounted () {
+
+  },
+	props: {
+	},
+	methods : {
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/Creator.vue
+++ frontend/src/views/Creator.vue
@@ -1,125 +1,61 @@
 <template>
   <div>
-  <div id="editor"></div>
-  <button id="submit">제출</button>
-  <div id="viewer"></div>
-  <div style="height: 100vh ; width : 100vw;">
-    <div id="tui-image-editor" ></div>
-  </div>
-  <button id="imgsubmit">제출</button>
-
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3"><Sidebar/></div>
+        <div v-if="currentAtiveFilter === 'info'" class="col-lg-9"><Info/></div>
+        <div v-if="currentAtiveFilter === 'thumbnail'" class="col-lg-9"><Thumbnail/></div>
+        <div v-else-if="currentAtiveFilter === 'course'" class="col-lg-9"><Class/></div>
+        <div v-else-if="currentAtiveFilter === 'kit'" class="col-lg-9"><Kit/></div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
+// Components import
+import Sidebar from '@/components/makeCourse/Sidebar.vue';
+import Class from '@/components/makeCourse/Class.vue';
+import Kit from '@/components/makeCourse/Kit.vue';
+import Thumbnail from '@/components/makeCourse/Thumbnail.vue';
+import Info from '@/components/makeCourse/Info.vue';
 
-// TOAST UI Editor import
-import Editor from '@toast-ui/editor';
-import _Editor from '@toast-ui/editor/dist/toastui-editor.js';
-import Viewer from '@toast-ui/editor/dist/toastui-editor-viewer';
-import ImageEditor from 'tui-image-editor';
-// TOAST UI Editor Plugins
-import 'codemirror/lib/codemirror.css';
-import '@toast-ui/editor/dist/toastui-editor.css';
-import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
-import 'tui-color-picker/dist/tui-color-picker.css';
-import 'tui-image-editor/dist/tui-image-editor.css'
 
 export default {
+  components : {
+    Sidebar,
+    Class,
+    Kit,
+    Thumbnail,
+    Info,
+  },
   data() {
     return {
-      editContent : '',
-      imgEditor : '',
+      currentAtiveFilter : '',
     }
   },
+  watch : {
+    $route(to, from) { 
+      if (to.path != from.path) { 
+        this.checkQuery();
+      } 
+    }
+  },
+  created () {
+    this.checkQuery();
+  },
   mounted() {
-    const editor = new Editor({
-      el: document.querySelector("#editor"),
-      initialEditType: "wysiwyg",
-      previewStyle: "tab",
-      height: "500px",
-      plugins: [colorSyntax],
-      hooks: {
-        addImageBlobHook: function (blob,callback) {
-          console.log('커스텀했어요')
-          console.log('blob',blob)
-          console.log('callback',callback)
-          const reader = new FileReader();
-          reader.onload = event => {
-            callback(event.target.result);
-          };
-          reader.readAsDataURL(blob);
-
-          return false
-        }
-    },
-    });
-    const instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
-      includeUI: {
-        initMenu: 'filter',
-        menuBarPosition: 'right',
-      },
-      cssMaxWidth: '1000',
-      cssMaxHeight: '800',
-      usageStatistics: false,
-
-    });
-    this.imgEditor = instance;
-    const saveBtn = document.querySelector('.tui-image-editor-header-buttons .tui-image-editor-download-btn')
-    saveBtn.innerText = 'Save'
-    var myblob = new Blob([instance.toDataURL().split(',')[1]], {
-    type: 'text/plain'
-    });
-    console.log(editor.options.hooks.addImageBlobHook)
-    console.dir(editor.options.hooks.addImageBlobHook)
-    saveBtn.addEventListener('click', () => {
-      editor.options.hooks.addImageBlobHook(myblob,(url, text) => _Editor._proto._applyImage(url, text))
-    })
-    // document.querySelector("#submit").addEventListener("click", () => {
-    //   this.editContent = editor.getMarkdown()
-    //   this.getViewer()
-    // })
-    // document.querySelector("#imgsubmit").addEventListener("click", () => {
-    //   console.log('instance.toDataURL()',instance.toDataURL())
-    // })
-
   },
   methods : {
-    onEditorChange() {
-      console.log('onEditorChange')
-    },
-    onEditorStateChange() {
-      console.log('onEditorStateChange')
-    },
-    getHtml() {
-      let html = this.$refs.toastuiEditor.invoke('getHtml');
-      console.log(html)
-    },
-    getViewer() {
-      new Viewer({
-        el: document.querySelector('#viewer'),
-        initialValue: this.editContent,
-      });
+    checkQuery() {
+      this.currentAtiveFilter = this.$route.params.type ? this.$route.params.type : 'info';
     }
   }
 }
 </script>
 
 <style scoped>
-.imageEditorApp {
-  width: 1000px;
-  height: 800px;
-}
-.tui-image-editor-save-btn {
-    position: absolute;
-    left: 0;
-    right: 0;
-    display: inline-block;
-    top: 0;
-    bottom: 0;
-    width: 100%;
-    cursor: pointer;
-    opacity: 1;
-}
+
+
 </style>
