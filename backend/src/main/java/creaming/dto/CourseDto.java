982b9bdbdb6230c775a9162c86c1beb2bbbf7200
김다윤 <ekdbs628@gmail.com>
2021-05-02 16:55:23 +0900

    [feat] Update Dto
    CourseDto & RegisterDto constructor 및 toEntity 추가

--- backend/src/main/java/creaming/dto/CourseDto.java
+++ backend/src/main/java/creaming/dto/CourseDto.java
@@ -1,6 +1,8 @@
 package creaming.dto;
 
+import creaming.domain.course.Course;
 import creaming.domain.etc.FoodType;
+import creaming.domain.register.Register;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,11 +24,26 @@ public class CourseDto {
         private Long courseId;
         private MemberDto.SimpleProfile profile; // 강사 프로필
         private String name;
-        private LocalDateTime date;
+        private LocalDate date;
         private Integer price;
         private Integer rating;
+        // TODO : List<CourseFile> Type?
         private String image;
         private FoodType category;
+
+        public SimpleResponse(Course course) {
+            this.courseId = course.getId();
+            this.profile = new MemberDto.SimpleProfile(course.getMember());
+            this.name = course.getName();
+            this.date = course.getDate();
+            this.price = course.getPrice();
+            // TODO : 우리가 계산해서 넣는 정보?
+            this.rating = course.getRating();
+            // TODO : file?
+            // this.image
+            this.category = course.getCategory();
+        }
+
     }
 
     @Getter
--- backend/src/main/java/creaming/dto/RegisterDto.java
+++ backend/src/main/java/creaming/dto/RegisterDto.java
@@ -1,6 +1,8 @@
 package creaming.dto;
 
+import creaming.domain.delivery.Delivery;
 import creaming.domain.etc.Address;
+import creaming.domain.register.Register;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,6 +27,16 @@ public class RegisterDto {
 
         private Integer paidPrice; // register 의 price
         private LocalDateTime paidDate; // register 의 createdDate
+
+        public SimpleResponse(Register register) {
+            this.registerId = register.getId();
+            this.courseId = register.getCourse().getId();
+            this.memberId = register.getMember().getId();
+            this.course = new CourseDto.SimpleResponse(register.getCourse());
+            this.paidPrice = register.getPrice();
+            this.paidDate = register.getCreatedDate();
+        }
+
     }
 
     @Getter
@@ -45,6 +57,21 @@ public class RegisterDto {
         private String orderNotes;
         private String deliveryCompany;
         private String deliveryNumber;
+
+        public DetailResponse(Register register) {
+            // TODO : SimpleReponse와 동일한 코드 6줄,, SimpleResponse 쓰면 감싸서 보내게 됨
+            this.registerId = register.getId();
+            this.courseId = register.getCourse().getId();
+            this.memberId = register.getMember().getId();
+            this.course = new CourseDto.SimpleResponse(register.getCourse());
+            this.paidPrice = register.getPrice();
+            this.paidDate = register.getCreatedDate();
+            this.address = register.getDelivery().getAddress();
+            this.orderNotes = register.getDelivery().getNotes();
+            this.deliveryCompany = register.getDelivery().getCompany();
+            this.deliveryNumber = register.getDelivery().getNumber();
+        }
+
     }
 
     @Getter
@@ -60,9 +87,14 @@ public class RegisterDto {
         private Address address;
         private String orderNotes;
 
+        public Register toEntity() {
+            return Register.builder()
+                    .dayOfWeek(this.dayOfWeek)
+                    .price(this.paidPrice)
+                    .delivery(new Delivery(this.address, this.orderNotes))
+                    .build();
+        }
+
     }
-
-
-
 
 }
