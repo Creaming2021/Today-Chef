afd01c4b8c876f1d4552b9ab2251183ad56d5685
성진옥 <wlsdhr0831@naver.com>
2021-05-03 01:54:06 +0900

    [feat] make event api

---
+++ frontend/src/api/event.js
@@ -0,0 +1,44 @@
+import { basic, image } from '@/api/user.js';
+
+export const getEventList = async () => {
+	basic.get('events')
+		.then(res => res)
+		.catch(e => { console.log(e); });
+}
+
+export const postEvent = async ( event ) => {
+	basic.post('events', event)
+		.then(res => res)
+		.catch(e => { console.log(e); });
+}
+
+export const getEvent = async ( eventId ) => {
+	basic.get(`events/${eventId}`)
+		.then(res => res)
+		.catch(e => { console.log(e); });
+}
+
+export const putEvent = async ( event ) => {
+	basic.put(
+		`events/${event.id}`, 
+		{
+			content : event.content,
+			title : event.title,
+		});
+}
+
+export const deleteEvent = async ( eventId ) => {
+	basic.delete(`events/${eventId}`)
+		.then(res => res)
+		.catch(e => { console.log(e); });
+}
+
+export const postEventImage = async ( event ) => {
+	image.post(
+		`events/${event.id}/image`,
+		{
+			file: event.image,
+		}
+	).then(res => res)
+		.catch(e => { console.log(e); });
+}
