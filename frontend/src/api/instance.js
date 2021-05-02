ef734f95f98cf41b802829b5616e07e02cd3f179
성진옥 <wlsdhr0831@naver.com>
2021-05-03 01:27:51 +0900

    [feat] make user api

--- frontend/src/api/instance.js
+++ frontend/src/api/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = 'http://localhost:8080/api/';
+axios.defaults.baseURL = 'http://k4b204.p.ssafy.io/';
 // axios.defaults.baseURL = 'api/';
 // axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.responseType = 'json';
--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,4 +1,4 @@
-// import { basic } from '@/api/user.js';
+import { basic } from '@/api/user.js';
 
 export const checkUserDB =  async ( kakaoId ) => {
   console.log(`${kakaoId}로 db에 회원있는지 물어봅니다.`);
@@ -6,12 +6,32 @@ export const checkUserDB =  async ( kakaoId ) => {
 }
 
 export const signUp = async ( userInfo ) => {
-  console.log(userInfo);
-  console.log('로 회원가입 시킵니다.');
-  return await true;
+  basic.post('user/signup', userInfo)
+    .then(res => res)
+    .catch(e => { console.log(e); });
+
+  // console.log(userInfo);
+  // console.log('로 회원가입 시킵니다.');
+  // return await true;
 }
 
 export const signIn = async ( kakaoId ) => {
-  console.log(`${kakaoId}로 로그인 합니다.`);
-  return await true;
+  basic.post('user/login', kakaoId)
+    .then(res => res)
+    .catch(e => { console.log(e); });
+
+  // console.log(`${kakaoId}로 로그인 합니다.`);
+  // return await true;
+}
+
+export const checkNickname = async ( nickname ) => {
+  basic.get(
+    'user/check', 
+    {
+      params : { 
+        nickname 
+      }
+    })
+    .then(res => res)
+    .catch(e => { console.log(e); });
 }
