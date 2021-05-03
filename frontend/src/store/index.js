da295dc51af6be5b2edbc275faf4f58200615900
성진옥 <wlsdhr0831@naver.com>
2021-05-03 15:04:39 +0900

    [feat] make index store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import Vuex from "vuex";
 import home from './home.js';
 import user from './user.js';
+import event from './event.js';
+import review from './review.js';
+import course from './course.js';
+import member from './member.js';
+import qna from './qna.js';
 
 Vue.use(Vuex);
 
@@ -9,5 +14,10 @@ export default new Vuex.Store({
   modules: {
     home,
     user,
+    event,
+    review,
+    course,
+    member,
+    qna,
   }
 });
