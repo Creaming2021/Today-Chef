9588ccf3121d0c80e9cc0cc4b79f2a354fa01871
김다윤 <ekdbs628@gmail.com>
2021-05-05 22:48:06 +0900

    [feat] Add File constructor

---
+++ backend/src/main/java/creaming/domain/file/ProductReviewFileRepository.java
@@ -0,0 +1,6 @@
+package creaming.domain.file;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductReviewFileRepository extends JpaRepository<ProductReviewFile, Long> {
+}
