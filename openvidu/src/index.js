17286a2c6dd044a147764cc6cacaea3bc07c4fe5
차민석 <51734158+meloncha@users.noreply.github.com>
2021-05-05 10:06:07 +0900

    [test] 9

--- openvidu/src/index.js
+++ openvidu/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import VideoRoomComponent from './components/VideoRoomComponent';
+import VideoRoomComponent from './components/VideoRoomComponent';
 import registerServiceWorker from './registerServiceWorker';
-import Root from './Root';
 
 ReactDOM.render(
-  <Root />, document.getElementById('root')
+  <VideoRoomComponent />, document.getElementById('root')
 );
 registerServiceWorker();
