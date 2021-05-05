64bd70d856aeb634639022651da2768acbba2b1d
차민석 <51734158+meloncha@users.noreply.github.com>
2021-05-05 11:56:09 +0900

    [fix] Add constructor to Address

--- backend/src/main/java/creaming/domain/etc/Address.java
+++ backend/src/main/java/creaming/domain/etc/Address.java
@@ -1,8 +1,12 @@
 package creaming.domain.etc;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@NoArgsConstructor
 public class Address {
 
     private String address;
