2ed378e1583ddfa4860025d13d38a86244cfb4de
김다윤 <ekdbs628@gmail.com>
2021-05-05 22:46:11 +0900

    [fix] Modify request mapping

--- backend/src/main/java/creaming/controller/RootController.java
+++ backend/src/main/java/creaming/controller/RootController.java
@@ -1,12 +1,13 @@
 package creaming.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value="/")
 public class RootController {
 
+    @GetMapping("/")
     public String homeRedirect() {
         return "redirect:/swagger-ui/index.html";
     }
