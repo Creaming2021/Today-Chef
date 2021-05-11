e93a6be6f130b58e4d7b4d4905e7da3e5747bc9f
김다윤 <ekdbs628@gmail.com>
2021-05-11 23:45:12 +0900

    [feat] Add chat api

--- frontend/src/api/instance.js
+++ frontend/src/api/instance.js
@@ -5,10 +5,11 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/json';
 // axios.defaults.responseType = 'json';
 // axios.defaults.validateStatus = (status) => status >= 200 && status < 300;
-// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 const baseURL = 'https://k4b204.p.ssafy.io/api/';
 const baseURLImage = 'https://k4b204.p.ssafy.io/api/images/';
+const baseURLChat = 'https://k4b204.p.ssafy.io/chat/';
 
 export const basic = axios.create({
   baseURL,
@@ -21,5 +22,12 @@ export const image = axios.create({
   baseURLImage,
   headers: {
     'Content-Type': 'multipart/form-data',
+  },
+});
+
+export const chat = axios.create({
+  baseURL: baseURLChat,
+  headers: {
+    'Content-Type': 'application/json',
   },
 });
