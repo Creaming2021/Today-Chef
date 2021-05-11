a47b4ade2b9261b9126bd9c9883d1428d697eb86
김다윤 <ekdbs628@gmail.com>
2021-05-12 00:17:20 +0900

    [feat] Add log info

--- backend/src/main/java/creaming/controller/UserController.java
+++ backend/src/main/java/creaming/controller/UserController.java
@@ -24,6 +24,7 @@ public class UserController {
     @PostMapping("/login")
     @Operation(summary = "로그인")
     public ResponseEntity<UserDto.LoginResponseDto> login(@RequestBody @Valid UserDto.LoginRequestDto dto) {
+        log.info("(Post) login - kakaoId : {}", dto.getKakaoId());
         UserDto.LoginResponseDto result = userService.login(dto.getKakaoId());
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
@@ -31,6 +32,7 @@ public class UserController {
     @GetMapping("/email")
     @Operation(summary = "이메일 중복 확인")
     public ResponseEntity<Boolean> checkEmail(@RequestParam String email) {
+        log.info("(Get) checkEmail - email : {}", email);
         Boolean result = userService.checkEmail(email);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
@@ -38,6 +40,7 @@ public class UserController {
     @GetMapping("/nickname")
     @Operation(summary = "닉네임 중복 확인")
     public ResponseEntity<Boolean> checkNickname(@RequestParam String nickname) {
+        log.info("(Get) checkNickname - nickname : {}", nickname);
         Boolean result = userService.checkNickname(nickname);
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
@@ -45,6 +48,8 @@ public class UserController {
     @PostMapping("/signup")
     @Operation(summary = "회원 가입")
     public ResponseEntity<UserDto.LoginResponseDto> signup(@RequestBody @Valid UserDto.SignupRequestDto dto) {
+        log.info("(Post) signup - kakaoId : {} | email : {}, nickname : {}, phone : {}"
+                        , dto.getKakaoId(), dto.getEmail(), dto.getNickname(), dto.getPhone());
         UserDto.LoginResponseDto result = userService.signUp(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
