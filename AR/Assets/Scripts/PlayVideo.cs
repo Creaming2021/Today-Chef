66cf29bcfe99aec8a048364002174d3966551e1d
성진옥 <wlsdhr0831@naver.com>
2021-05-16 19:19:53 +0900

    [feat] change video source and camera auto focus
    Former-commit-id: c8435b48e7e8b4c1359a33cb5fcf38be0e2cc9c6

--- AR/Assets/Scripts/PlayVideo.cs
+++ AR/Assets/Scripts/PlayVideo.cs
@@ -5,24 +5,54 @@ using UnityEngine.Video;
 
 public class PlayVideo : MonoBehaviour
 {
-  public GameObject gameObject;
+  public GameObject playButton;
+  public GameObject video;
 
-  public void DoDelayAction()
-  {
-    StartCoroutine(DelayAction(1));
-  }
+  // public void DoDelayAction()
+  // {
+  //   StartCoroutine(DelayAction(1));
+  // }
  
-  IEnumerator DelayAction(float delayTime)
-  {
-    //Wait for the specified delay time before continuing.
-    yield return new WaitForSeconds(delayTime);
+  // IEnumerator DelayAction(float delayTime)
+  // {
+  //   //Wait for the specified delay time before continuing.
+  //   yield return new WaitForSeconds(delayTime);
   
-    //Do the action after the delay time has finished.
-    Play();
+  //   //Do the action after the delay time has finished.
+  //   Play();
+  // }
+
+  public void Pause(){
+    video.GetComponent<VideoPlayer>().Pause();
+    video.SetActive(false);
+    Debug.Log("멈춤");
   }
 
   void Play(){
-    gameObject.SetActive(true);
-    gameObject.GetComponent<VideoPlayer>().Play();
+    playButton.SetActive(false);
+    video.SetActive(true);
+    // video.GetComponent<VideoPlayer>().Play();
   }
+
+  void FixedUpdate () {
+    if(Input.GetMouseButton(0)){
+      GameObject target = GetClickedObject();
+      Debug.Log("클릭");
+      if(target != null && target.Equals(playButton)){
+        Debug.Log("재생");
+        Play();
+      }
+    }
+  }
+  
+  GameObject GetClickedObject() {
+    GameObject target = null; 
+    RaycastHit hit;
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //마우스 포인트 근처 좌표를 만든다. 
+
+    if(Physics.Raycast(ray.origin, ray.direction * 10, out hit)){
+      target = hit.collider.gameObject; 
+    } 
+    return target; 
+  } 
 }
--- AR/Assets/Scripts/TouchItem.cs
+++ AR/Assets/Scripts/TouchItem.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TouchItem : MonoBehaviour
 {
+    public GameObject gameObject;
     // Start is called before the first frame update
     void Start()
     {
