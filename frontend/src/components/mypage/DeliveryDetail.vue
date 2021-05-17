281b78d7ceac152b2f66b08bfe26fa944a0bf6b6
김다윤 <ekdbs628@gmail.com>
2021-05-18 02:20:42 +0900

    [feat] Add css style

--- frontend/src/components/mypage/DeliveryDetail.vue
+++ frontend/src/components/mypage/DeliveryDetail.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="shopping__cart__table">
-    <div>{{payment.date}} 주문 ({{payment.date}}-{{payment.orderId}})</div>
-    <div>주문 상세</div>
-    <div class="quantity">
+    <div class="order">주문 상세정보</div>
+    <div class="order-detail">
+      <div class="order-detail-info"> 주문 일자 {{payment.date.substring(0, 10)}}</div> |
+      <div class="order-detail-info"> 주문번호 {{payment.date.substring(0, 10)}}-{{payment.orderId}} </div>
+    </div>
+    <div class="info-title">상품 정보</div>
+    <div class="order-info-details">
         <div class="pro-qty-2" v-for="detail in payment.orderDetails" :key="detail.product.id">
-          <h5>{{detail.product.category}}</h5>
-          <img :src="detail.product.image" />
-          <h5>{{detail.product.name}}</h5>
-          <h5>{{detail.product.price}}원</h5>
-          <h5>{{detail.amount}}개</h5>
+          <img :src="detail.product.image" class="order-info-image"/>
+          <div class="order-info-detail">
+            <div><strong>카테고리</strong> : {{detail.product.category}}</div>
+            <div><strong>상품명</strong> : {{detail.product.name}}</div>
+            <div><strong>상품금액 (수량)</strong> : {{detail.product.price}}원 ({{detail.amount}}개)</div>
+          </div>
         </div>
     </div>
 
-    <div>배송지 정보</div>
-    <table>
+    <div class="info-title">배송지 정보</div>
+    <table class="delivery-info">
       <thead>
         <tr>
           <th>받는 사람</th>
@@ -51,5 +56,46 @@ export default {
 }
 .delivery-detail:hover {
   text-decoration: underline;
+}
+.order {
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 0 0 5px 4px;
+}
+.order-detail {
+  border: 4px solid lightgrey;
+  padding: 18px 15px 13px 15px;
+  display: flex;
+  font-size: 1rem;
+  font-weight: bold;
+}
+.order-detail-info {
+  padding: 0 10px 3px 10px;
+}
+.info-title {
+  font-size: 1.3rem;
+  font-weight: bold;
+  margin: 30px 0 10px 4px;
+}
+.order-info-image {
+  display: inline;
+  width: 200px;
+}
+.order-info-detail {
+  position: absolute;
+  display: inline-block;
+  padding: 0 1rem;
+}
+.order-info-detail div {
+  position: relative;
+  left: 5px;
+  top: 10px;
+  padding-top: 10px;
+}
+.delivery-info {
+  margin: 8px;
+}
+td {
+  font-size: 14px;
 }
 </style>
--- frontend/src/components/mypage/DeliveryTracker.vue
+++ frontend/src/components/mypage/DeliveryTracker.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <p 
-      class="goToDetail"
-      @click="changeModalType('detail')">상세보기로 돌아가기</p>
-
-    <div>{{deliveryTrack.state.text}}</div>
-    <div>
-      <div>{{deliveryTrack.carrier.name}}</div>
-      <div>{{deliveryTrack.carrier.tel}}</div>
+    <div class="tracker-header goToDetail"
+        @click="changeModalType('detail')">
+      <ion-icon ios="ios-backspace" md="md-backspace" class="icon"></ion-icon>
+      <p>상세보기로 돌아가기</p>
+    </div>
+    <div class="tracker-body">
+      <h3>{{deliveryTrack.state.text}}</h3>
+      <div class="tracker-body-company">
+        <div>{{deliveryTrack.carrier.name}}</div>
+        <div>{{deliveryTrack.carrier.tel}}</div>
+      </div>
     </div>
 
-    <div>
+    <div class="shopping__cart__table">
       <table>
         <thead>
           <tr>
@@ -22,7 +25,8 @@
         <tbody>
           <template v-for="(progress, idx) in deliveryTrack.progresses">
             <tr :key="idx">
-              <td>{{progress.time}}</td>
+              <td>{{progress.time.substring(0,10)}} {{progress.time.substring(11,19)}}
+              </td>
               <td>{{progress.location.name}}</td>
               <td>{{progress.status.text}}</td>
             </tr>
@@ -42,12 +46,42 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 table{
   width: 100%;
 }
-
+.tracker-header {
+  display: flex;
+}
+.tracker-header p{
+  font-size: 1rem;
+  font-weight: bold;
+}
 .goToDetail:hover {
   cursor: pointer;
+}
+.tracker-body {
+  text-align: center;
+}
+.tracker-body-company {
+  display: inline-block;
+  padding: 5px 0;
+}
+.tracker-body-company div{
+  display: inline;
+  margin: 0 10px;
+}
+.shopping__cart__table {
+  margin-top: 60px;
+}
+.shopping__cart__table table tbody tr td ion-icon {
+  padding: 3px;
+}
+.shopping__cart__table table tbody tr td {
+    padding-bottom: 20px;
+    padding-top: 20px;
+}
+.shopping__cart__table table tr {
+  text-align: center;
 }
 </style>
